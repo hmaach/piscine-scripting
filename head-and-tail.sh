49522454c2e3c3db82f5424392644a4965e4d255
Hamza Maach <hamzamaach56@gmail.com>
2026-01-19 16:11:42 +0100

    head and tails of content

--- head-and-tail.sh
+++ head-and-tail.sh
@@ -1,2 +1,2 @@
-curl -s https://assets.01-edu.org/devops-branch/HeadTail.txt | head -1
-curl -s https://assets.01-edu.org/devops-branch/HeadTail.txt | tail -1 
+curl -s  https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt | head -1
+curl -s  https://learn.zone01oujda.ma/assets/devops-branch/HeadTail.txt | tail -1 
