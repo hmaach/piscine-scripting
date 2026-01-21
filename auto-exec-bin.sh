3a64a6f34bd5c9bd94115ffec92840c55a794161
Hamza Maach <hamzamaach56@gmail.com>
2026-01-21 18:23:31 +0100

    read last process exit code

--- auto-exec-bin.sh
+++ auto-exec-bin.sh
@@ -1,12 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-BIN_DIR="$HOME/myBins"
-SHELL_RC="$HOME/.bashrc"
-
-# Make sure the binary is executable
-chmod +x "$BIN_DIR/01exec" 2>/dev/null
-
-# Add directory to PATH if not already there
-grep -q 'export PATH="$HOME/myBins:$PATH"' "$SHELL_RC" || \
-printf '%s\n' 'export PATH="$HOME/myBins:$PATH"' >> "$SHELL_RC"
+export PATH="$HOME/myBins:$PATH"
 
