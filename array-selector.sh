be68e0be94144b3841c5146ca9a5bf55d20c6a4f
Hamza Maach <hamzamaach56@gmail.com>
2026-01-23 17:59:50 +0100

    read last process exit code

---
+++ array-selector.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Declare the array
+colors=("red" "blue" "green" "white" "black")
+
+if [[ $# -ne 1 ]]; then
+    echo "Error"
+    exit 1
+fi
+
+if [[ ! $1 =~ ^[0-9]+$ ]]; then
+    echo "Error"
+    exit 1
+fi
+
+index=$1
+
+if (( index < 1 || index > ${#colors[@]} )); then
+    echo "Error"
+    exit 1
+fi
+
+echo "${colors[index-1]}"
+
