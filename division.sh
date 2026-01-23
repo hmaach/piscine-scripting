5ddc9a21e7f6bf18ac177a25aaffe992e9f79afe
Hamza Maach <hamzamaach56@gmail.com>
2026-01-23 17:39:15 +0100

    read last process exit code

--- division.sh
+++ division.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]]; then
-  echo "Error: two numbers must be provided."
-  exit 1
+is_integer(){
+    if [[ $1 =~ ^[0-9]+$ ]];
+        then
+            return 0
+    else
+        return 1
+    fi
+}
+if [[ $# -ne 2 ]]
+    then 
+        echo "Error: two numbers must be provided";
+else
+
+    if ! is_integer "$1" || ! is_integer "$2";
+        then
+            echo "Error: both arguments must be integers";
+    else
+        if [[ $2 -eq 0 ]];
+            then
+                echo "Error: division by zero is not allowed"
+        else
+           echo "$1 / $2" | bc
+        fi
+    fi
 fi
-
-a="$1"
-b="$2"
-
-if ! [[ "$a" =~ ^-?[0-9]+$ ]] || ! [[ "$b" =~ ^-?[0-9]+$ ]]; then
-  echo "Error: both arguments must be integers."
-  exit 1
-fi
-
-if [[ "$b" == "0" ]]; then
-  echo "Error: division by zero is not allowed."
-  exit 1
-fi
-
-echo "scale=0; $a / $b" | bc
