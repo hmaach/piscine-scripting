4971ff3e159ef9b782dc36dae8989e4a35e9bd6a
Hamza Maach <hamzamaach56@gmail.com>
2026-01-24 12:47:56 +0100

    read last process exit code

--- joker-num.sh
+++ joker-num.sh
@@ -12,33 +12,32 @@ if ! [[ "$secret" =~ ^[0-9]+$ ]] || [ "$secret" -lt 1 ] || [ "$secret" -gt 100 ]
     exit 1
 fi
 
-tries=5
+tries_left=5
+moves=0
 found=0
 
-for ((i=1; i<=5; i++)); do
-    echo "Enter your guess ($tries tries left):"
+for (( ; tries_left > 0; )); do
+    echo "Enter your guess ($tries_left tries left):"
     read guess
 
-    # Invalid input: empty or non-numeric
-    if [[ -z "$guess" ]] || ! [[ "$guess" =~ ^[0-9]+$ ]]; then
-        i=$((i - 1))   # do not consume a try
+    if [[ -z "$guess" ]] || ! [[ "$guess" =~ ^[0-9]+$ ]] || [ "$guess" -lt 1 ] || [ "$guess" -gt 100 ]; then
         continue
     fi
+
+    moves=$((moves + 1))
+    tries_left=$((tries_left - 1))
 
     if [ "$guess" -gt "$secret" ]; then
         echo "Go down"
     elif [ "$guess" -lt "$secret" ]; then
         echo "Go up"
     else
-        echo "Congratulations, you found the number in $i moves!"
+        echo "Congratulations, you found the number in $moves moves!"
         found=1
         break
     fi
-
-    tries=$((tries - 1))
 done
 
 if [ "$found" -eq 0 ]; then
     echo "You lost, the number was $secret"
 fi
-
