3b93593ae6678a70f67974ecd47fa14a973fa735
Hamza Maach <hamzamaach56@gmail.com>
2026-01-29 11:52:43 +0100

    python function

---
+++ custom_calendar.py
@@ -0,0 +1,19 @@
+def day_from_number(day_number):
+
+    if day_number < 1 or day_number > 7:
+        return "None"
+
+    days = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+    
+    return days[day_number - 1]
+
+
+def day_to_number(day):
+   
+    days = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+    
+    for i, d in enumerate(days):
+        if d == day:
+            return i + 1
+
+    return "None"
