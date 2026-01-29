a9b27dacea7507cce44ffdb35da802a98c063936
Hamza Maach <hamzamaach56@gmail.com>
2026-01-29 18:09:27 +0100

    python function

--- string_processing.py
+++ string_processing.py
@@ -3,7 +3,7 @@ import string
 def tokenize(sentence):
     result = []
     
-    translator = str.maketrans('', '', string.punctuation)
-    s = sentence.lower().replace("'", " ").translate(translator) 
+    translator = str.maketrans(string.punctuation, " " * len(string.punctuation))
+    s = sentence.lower().translate(translator) 
 
     return s.split()
